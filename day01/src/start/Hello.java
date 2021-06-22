b20deb20598563d515b939f2d84da88e926a71bf
kimseyoung1 <42962681+kimseyoung1@users.noreply.github.com>
2021-06-22 18:04:56 +0900

    Add files via upload

---
+++ day01/src/start/Hello.java
@@ -0,0 +1,9 @@
+package start;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("나는 자동완성입니다.");
+		System.out.println("환영합니다.");
+		System.out.println("반갑습니다.");
+	}
+}
